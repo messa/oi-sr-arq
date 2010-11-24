81e7d79fb096d2d5c02ac9ba2f1437932bd40e4a
janfabian <jan.fabian@jetminds.com>
2010-11-24 02:38:57 -0800

    comment

--- client.c
+++ client.c
@@ -1,8 +1,12 @@
+/*
+This project was created within course Operating Systems and Networks
+at FEE CTU, fall semester 2010/2011.
+
+Authors: Petr Messner, Jan Fabian
+*/
 
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
     printf("Hello World from client\n");
 }
-
-
